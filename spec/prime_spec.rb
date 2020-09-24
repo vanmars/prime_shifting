345ce3a8533226e13aed2b64f01c2ae6f0d5a724
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-24 11:06:07 -0700

    Write and FAIL test for spec 2

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -6,4 +6,9 @@ describe('Prime#prime_sift') do
     prime = Prime.new(12)
     expect(prime.prime_sift).to(eq([2,3,4,5,6,7,8,9,10,11,12]))
   end
+
+  it('will return an array of numbers between 2 and the inputted number, with prime multiples removed') do
+    prime = Prime.new(12)
+    expect(prime.prime_sift).to(eq([2,3,5,7,9,11]))
+  end
 end
